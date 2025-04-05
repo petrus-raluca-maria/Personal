7d0df0b83b5ae02b40001219536610b6a38ff3ee
petrus-raluca-maria <161299556+petrus-raluca-maria@users.noreply.github.com>
2025-04-05 14:01:30 +0300

    Reorder

--- Licenta/scripts/animation.js
+++
@@ -1,7 +0,0 @@
-export function waitUntilAnimationsFinish(element) {
-  const animationPromises = element
-    .getAnimations()
-    .map((animation) => animation.finised);
-
-  return Promise.allSettled(animationPromises);
-}
--- Licenta/scripts/event-form.js
+++
@@ -1,9 +0,0 @@
-export function initEventForm() {
-  const formElement = document.querySelector("[data-event-form]");
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log("Form submited");
-  });
-
-  return {};
-}
